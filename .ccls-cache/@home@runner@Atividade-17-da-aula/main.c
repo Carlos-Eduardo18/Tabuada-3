5c06ce69cdda3a8ae1af3f72281604b622cfa125
Carlos-Eduardo18 <carlos18.morais18@gmail.com>
2024-06-21 17:51:24 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Atividade-17-da-aula/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int i, r;
+
+  printf("Tabuada do 3\n");
+  for(i = 1; i <= 10; i++){
+    r = 3 * i;
+  printf("3 X %d = %d\n", i, r);
+  }
+}
